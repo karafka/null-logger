0b7bdfd06bef34563f8b6600d7f34ba37e302bc6
Adam Gwozdowski <agwozdowski@striketech.pl>
2015-09-04 17:20:23 +0200

    Logger with log levels checking

--- lib/null_logger/logger.rb
+++ lib/null_logger/logger.rb
@@ -2,9 +2,14 @@ module NullLogger
   # Null logger class
   # Is used when logger is not defined
   class Logger
-    # Returns nil for any method call
-    def method_missing(*_args, &_block)
-      nil
+    # Possible log levels from ruby Logger::Severity class
+    LOG_LEVELS = %w( unknown fatal error warn info debug )
+
+    # Returns nil for any method call from LOG_LEVELS array
+    # Instead raise NoMethodError
+    def method_missing(method_name, *args, &block)
+      return nil if LOG_LEVELS.include?(method_name.to_s)
+      super(method_name, *args, &block)
     end
   end
 end
--- spec/lib/null_logger/logger_spec.rb
+++ spec/lib/null_logger/logger_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe NullLogger::Logger do
-  pending
+  subject { described_class.new }
+
+  it 'return nil if call method from LOG_LEVELS array' do
+    expect(subject.warn).to eq nil
+  end
+
+  it 'raise exception if call method which not exist in LOG_LEVELS array' do
+    expect { subject.warnnnn }.to raise_error(NoMethodError)
+  end
 end
